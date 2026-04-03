e6473bf5e682ef083da458a303d00eec4b521ae7
scott-lang24 <zenwhispers9085@gmail.com>
2026-04-03 13:52:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import upload
 from database.config import engine, Base
 
-# This creates all your database tables automatically
+# Creates all database tables automatically
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="LedgerFlux Core API")
 
-# This plugs your batch upload route into the main server
+# --- ADD THIS CORS BLOCK ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins for local testing. We will restrict this in production.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# ---------------------------
+
+# Plugs your batch upload route into the main server
 app.include_router(upload.router)
 
 @app.get("/")
