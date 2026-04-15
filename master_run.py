6f0350e2fc70c34198bbb2c58f995f7dcc26dd01
scott-lang24 <zenwhispers9085@gmail.com>
2026-04-15 10:53:44 +0000

    faah

--- master_run.py
+++ master_run.py
@@ -1,13 +1,3 @@
-from supabase import create_client, Client
-
-# --- SUPABASE INIT ---
-@st.cache_resource
-def init_supabase():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
-
-supabase = init_supabase()
 import pandas as pd
 import zipfile
 import sqlite3
